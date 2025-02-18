fe8046a3356663728c9c908017ac5b0068d887f3
scarlet <scarryaa@gmail.com>
2025-02-18 10:02:16 -0500

    Add tree-sitter bindings

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # meteor
 
 A modern editor.
+
+## Build Instructions
+Run `bash
+flutter pub run ffigen --config ffigen.yaml`
+to generate the tree-sitter bindings.
---
+++ lib/shared/providers/tree_sitter.dart
@@ -0,0 +1,11 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'tree_sitter.g.dart';
+
+@riverpod
+class TreeSitter extends _$TreeSitter {
+  @override
+  void build() {
+    return;
+  }
+}
